0475a2a2d73a13376d3a669c2c7d1f15768dd554
jujuck <julien.y.richard@gmail.com>
2018-04-08 17:07:20 +0200

    gestion de l'affichage points, diff, nbtry et chrono

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,43 @@
 //création des variables
-var numBoucle = 0;
 var tour = 1;
 var points = 0;
-var canPlay = true;
+var nbTry = 0;
+var numBoucle = 0;
+var difficulte = 1;
+var canPlay = true; //Enable et disable l'appel au onclick
 var carteJouée1;
 var carteJouée2;
+var secondes = 0;
+var minutes = 0;
 
+//Définition des élements d'affihcage à modifier
+var pointsElt = document.getElementById("points");
+var nbTryElt = document.getElementById("nbTry");
+var difficulteElt = document.getElementById("difficulte");
+var tpsJeuxElt = document.getElementById("tpsJeux");
+
+//fonction de mise à jour de l'affihcage du score
+function affichageScore() {
+    pointsElt.innerHTML = points;
+    nbTryElt.innerHTML = nbTry;
+    difficulteElt.innerHTML = difficulte;
+    if (points === 8) {
+        
+        alert("féliciations, vous avez réussi!!!");
+    }
+}
+
+//fonction chrono
+function chrono() {
+    setInterval(function () {
+        secondes ++;
+        if (secondes === 60){
+            minutes++;
+            secondes = 0;
+        }
+        tpsJeuxElt.innerHTML = minutes + " : " + secondes;
+    }, 1000);
+}
 
 //function de jouer
 function jouer(IdCarte, Value) {
@@ -22,9 +54,11 @@ function jouer(IdCarte, Value) {
         carteJouée2.classList.add("vu");
         carteJouée2.value = Value;
         
+        //Vérification des cartes et temps de mémorisation
         setTimeout(function() {
             if (carteJouée1.value === carteJouée2.value) {
                 points++;
+                console.log(points);
                 carteJouée1.classList.add("validee");
                 carteJouée2.classList.add("validee");
                 carteJouée1.onclick= null;
@@ -35,8 +69,10 @@ function jouer(IdCarte, Value) {
             }
             canPlay = true;
         }, 3000);
-        
         tour = 1;
+        nbTry++;
+        //Mise à jour de l'affichage
+        affichageScore();
     }
 }
 
@@ -70,5 +106,7 @@ function initCarte(numJeux) {
 
 function distribution() {
     initCarte(1);
+    affichageScore();
+    chrono();
 };
 
